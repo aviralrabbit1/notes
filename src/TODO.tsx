fb0fe53979036cffd25982a636bfb9524e84558f
Aviral Verma <aviral.robin@gmail.com>
2023-09-27 05:16:47 +0530

    Added Notes content body

--- src/TODO.tsx
+++ src/TODO.tsx
@@ -2,19 +2,22 @@ import { useState } from "react";
 
 interface Note {
     title: string;
+    body: string;
 }
 
 function TODO() {
     const [title, setTitle] = useState<string>("");
     const [notes, setNotes] = useState<Note[]>([]);
+    const [body, setBody] = useState("");
 
     const addNote = (event: { preventDefault: () => void; }) => {
         event.preventDefault();
         setNotes([
             ...notes,
-            { title }
+            { title, body }
         ])
         setTitle("");
+        setBody("");
     }
 
     const removeNote = (title: string) => {
@@ -29,11 +32,14 @@ function TODO() {
         {notes.map((note) => (
             <div key={note.title}>
                 <h3>{note.title} </h3>
+                <p>{note.body} </p>
                 <button onClick={() => removeNote(note.title)}>Remove Note</button>
             </div>
         ))}
+        <p>Add notes here</p>
         <form action="" onSubmit={addNote} >
             <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
+            <textarea value={body} onChange={(e) => setBody(e.target.value)} />
             <button>Add Note</button>
             <h3>{title} </h3>
         </form>
