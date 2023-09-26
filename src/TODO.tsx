73c84fd54fe72ffefbb0a41863824457c05f5e7f
Aviral Verma <aviral.robin@gmail.com>
2023-09-27 03:52:17 +0530

    basic form

--- src/TODO.tsx
+++ src/TODO.tsx
@@ -1,14 +1,22 @@
 import { useState } from "react";
 
 function TODO() {
-    const [text, settext] = useState("");
+    const [title, setTitle] = useState("");
+
+    const addNote = (event: { preventDefault: () => void; }) => {
+        event.preventDefault();
+    }
+
     return (
       <div>
         <h1>
           TODO app
         </h1>
-        <input type="text" onChange={(e) => settext(e.target.value)} />
-        <p>{text} </p>
+        <h3>Add Notes here</h3>
+        <form action="" onSubmit={addNote} >
+            <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
+            <button>Add Note</button>
+        </form>
       </div>
     )
 }
