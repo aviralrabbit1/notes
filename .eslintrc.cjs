66185d06b199921fd4eb4af764f8ad13e61565c0
Aviral Verma <aviral.robin@gmail.com>
2023-09-27 03:17:11 +0530

    Added production recommended stuff

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,28 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname,
+    "ecmaFeatures": {
+      "jsx": true
+    }
+   },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    `plugin:@typescript-eslint/stylistic-type-checked`,
     'plugin:react-hooks/recommended',
+    "plugin:react/recommended"
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: [
+    'react-refresh',
+    "react",
+  ],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
--- README.md
+++ README.md
@@ -4,7 +4,9 @@ Setup with Vite's `React+TypeScript+Vite` template with
 ```sh
 bun create vite@latest
 ```
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules. [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+- SWC is an extensible Rust-based platform for the next generation of fast developer tools. It's used by tools like Next.js, Parcel, and Deno. SWC can be used for both compilation and bundling.
+- This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
 
 ## Expanding the ESLint configuration
 
